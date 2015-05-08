1160bbe7abac9a09c63d996bc3f2ecd6d14022b6
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:43:03 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_edtaa3.py
+++ lutin_edtaa3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
-import lutinTools
+import lutin.module as module
+import lutin.tools as tools
 
 def get_desc():
 	return "edtaa3 library (create distance field from image)"
